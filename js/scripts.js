25058d08557dd434a7e0ce0456a1d59530f26710
IlarioCOCCO <ilario.cocco@gmail.com>
2021-09-27 16:16:07 +0200

    commit debug axios in vue js

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,26 @@
+// Descrizione:
+// Attraverso l'apposita API di Boolean
+// https://flynn.boolean.careers/exercises/api/random/mail
+// generare 10 indirizzi email e stamparli in pagina all'interno di una lista.
+// Bonus
+// Far comparire gli indirizzi email solamente quando sono stati tutti generati.
+
 const app = new Vue({
     el: '#root',
     data: {
-
+        "success" :	true,
+        "response":	"dewayne54@hansen.com"
     },
-    methods: {
 
+    methods: {
+        
+    
     },
     mounted(){
-        axios.get()
+        axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+        .then((response) => {
+            this.result.data = response.data.response;
+        }
+        )
     }
 })
